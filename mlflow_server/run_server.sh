2ee35d8b0189652a9e6b75532cba63536c6d33d5
Podckast <krolart2222@gmail.com>
2024-07-08 11:06:57 +0000

    git commit -m 'Создан mflow Tracking Server и зарегистрирована базовая модель

---
+++ mlflow_server/run_server.sh
@@ -0,0 +1,10 @@
+export MLFLOW_S3_ENDPOINT_URL=https://storage.yandexcloud.net
+export AWS_ACCESS_KEY_ID=YCAJE8CKXGh8XKUmcmGHpV2CX
+export AWS_SECRET_ACCESS_KEY=YCNQmCkW71PeIVypo2YsF-4eIHhiRBZZN94pztrj
+export AWS_BUCKET_NAME=s3-student-mle-20240523-9a4b29d5bb
+
+mlflow server \
+  --registry-store-uri postgresql://mle_20240523_9a4b29d5bb:6829393035904b068e86b9d3e2bc2f6d@rc1b-uh7kdmcx67eomesf.mdb.yandexcloud.net:6432/playground_mle_20240523_9a4b29d5bb \
+  --default-artifact-root s3://s3-student-mle-20240523-9a4b29d5bb \
+  --backend-store-uri postgresql://mle_20240523_9a4b29d5bb:6829393035904b068e86b9d3e2bc2f6d@rc1b-uh7kdmcx67eomesf.mdb.yandexcloud.net:6432/playground_mle_20240523_9a4b29d5bb \
+  --no-serve-artifacts
